9ec1a3e5974f83f5d828f9ba986e24628153709c
Violeta Saguier <violetasaguier@gmail.com>
2023-06-15 17:01:44 -0300

    otro push

--- java.js
+++ java.js
@@ -1,14 +1,32 @@
-let arrayProducto =[]
-let Total = 0
-function sumaProductos(Valoritem, Producto) {
-    arrayProducto.push(Producto)
-  Total = Total + Valoritem
-  document.getElementById("elTotal").innerHTML = "TOTAL: " + "$" + Total; 
-  console.log(arrayProducto);
-  document.getElementById("valorProducto").value = Total
-  document.getElementById("listaProductos").value=arrayProducto
-  return Total}
+// Definimos un objeto Carrito que contiene la lista de productos y su valor total
+var Carrito = {
+    productos: [],
+    valorTotal: 0,
+}
+  
+// Función para sumar productos
+function sumaProductos(ValorItem, Producto) {
+    // Añadimos el producto al carrito
+    Carrito.productos.push(Producto);
+  
+    // Sumamos el valor del producto al valor total del carrito
+    Carrito.valorTotal += ValorItem;
+  
+    // Actualiza la interfaz del carrito
+    actualizarCarritoUI();
+  }
+  
 
 function Limpiar() {
-  Total=0
-  document.getElementById("elTotal").innerHTML = "TOTAL: " + "$" + 0; }
+    Total=0
+    document.getElementById("elTotal").innerHTML = "TOTAL: " + "$" + 0; }
+
+    function actualizarCarritoUI() {
+        var carritoElemento = document.getElementById("elTotal");
+      
+        // Actualiza el monto total del carrito
+        carritoElemento.innerHTML = "Monto total: " + Carrito.valorTotal + " Pesos";
+      
+        // Aquí puedes agregar código para listar los productos en el carrito si lo deseas
+      }
+      
